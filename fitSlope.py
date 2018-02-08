39d5ab1b72cce2ae138b50299e612088c59b0c18
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-02-08 22:24:04 +0530

    found intercept for linear regression

--- fitSlope.py
+++ fitSlope.py
@@ -5,13 +5,22 @@ import matplotlib.pyplot as plt
 xs = np.array([1,2,3,4,5,6], dtype=np.float64)
 ys = np.array([5,4,6,5,6,7], dtype=np.float64)
 
-def best_fit_slope(xs, ys):
+def best_fit_slope_and_intercept(xs, ys):
     
     m = ( ( (mean(xs) * mean(ys)) - mean(xs*ys) ) /
             ((mean(xs)**2) - mean(xs**2)))
+    b = mean(ys) - m*mean(xs)
 
-    return m
+    return m, b
 
-slope = best_fit_slope(xs, ys)
+m,b = best_fit_slope_and_intercept(xs, ys)
 
-print(slope)
+linear_regression = [(m*x) + b for x in xs]
+
+predict_x = 8
+predict_y = (m * predict_x) + b
+
+plt.scatter(xs, ys)
+plt.scatter(predict_x, predict_y)
+plt.plot(xs, linear_regression)
+plt.show()
