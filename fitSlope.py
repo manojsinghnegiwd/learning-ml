dc0c7048ef36d0b43ac268e5c967195440323e9e
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-02-08 23:47:20 +0530

    created multiple data set and tested linear regression

--- fitSlope.py
+++ fitSlope.py
@@ -1,9 +1,25 @@
 from statistics import mean
 import numpy as np
+import random
 import matplotlib.pyplot as plt
 
-xs = np.array([1,2,3,4,5,6], dtype=np.float64)
-ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+#xs = np.array([1,2,3,4,5,6], dtype=np.float64)
+#ys = np.array([5,4,6,5,6,7], dtype=np.float64)
+
+def create_dataset(hm, variance, step=2, correlation=False):
+    val = 1
+    ys = []
+    for i in range(hm):
+        y = val + random.randrange(-variance, variance)
+        ys.append(y)
+        if correlation and correlation == 'pos':
+            val += step
+        elif correlation and correlation == 'neg':
+            val -= step
+
+    xs = [i for i in range(len(ys))]
+
+    return np.array(xs, dtype=np.float64), np.array(ys, dtype=np.float64)
 
 def best_fit_slope_and_intercept(xs, ys):
     
@@ -23,6 +39,8 @@ def coefficient_of_determination(ys_orig, ys_line):
     squared_error_regr = squared_error(ys_orig, ys_line)
     squared_error_y_mean = squared_error(ys_orig, y_mean_line)
     return 1 - (squared_error_regr / squared_error_y_mean)
+
+xs, ys = create_dataset(400, 200, 2, correlation='pos') 
 
 m,b = best_fit_slope_and_intercept(xs, ys)
 
