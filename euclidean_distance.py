303b3dd0bfc2a850acf0b152f41f40b8be4363e1
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-02-09 23:59:05 +0530

    caluclated the accuracy of k nearest neighbor

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -2,6 +2,8 @@ import numpy as np
 from math import sqrt
 import warnings
 from collections import Counter
+import pandas as pd
+import random
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= 3:
@@ -15,3 +17,38 @@ def k_nearest_neighbors(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
     return vote_result
+
+df = pd.read_csv("breast-cancer-wisconsin.data")
+df.replace('?', -99999, inplace=True)
+df.drop(['id'], 1, inplace=True)
+
+full_data = df.astype(float).values.tolist()
+random.shuffle(full_data)
+
+test_size = 0.2
+train_set = {2: [], 4: []}
+test_set = {2: [], 4: []}
+
+train_data = full_data[:-int(test_size*len(full_data))]
+test_data = full_data[-int(test_size*len(full_data)):]
+
+for i in train_data:
+    train_set[i[-1]].append(i[:-1])
+
+print(train_data[:10])
+
+for i in test_data:
+    test_set[i[-1]].append(i[:-1])
+
+correct = 0
+total = 0
+
+for group in test_set:
+    for data in test_set[group]:
+        vote = k_nearest_neighbors(train_set, data, k=5)
+        if group == vote:
+            correct += 1
+        total += 1
+
+print('Accuracy: ', correct/total)
+
