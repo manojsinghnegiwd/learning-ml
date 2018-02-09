6b59b753c42cc57171781cd49f5858725f06b94d
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-02-09 22:13:42 +0530

    plotted dataset using nested one liner loop

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,8 +1,27 @@
+import numpy as np
 from math import sqrt
+import matplotlib.pyplot as plt
+import warnings
+from matplotlib import style
+from collections import Counter
 
-plot1 = [1,3]
-plot2 = [2,5]
+style.use('fivethirtyeight')
 
-euclidean_distance = sqrt( (plot1[0] - plot2[0])**2 + (plot1[1] - plot2[1])**2 )
+dataset = {'k': [[1, 2],[2, 3],[3, 1]], 'r': [[6, 5],[7, 7],[8, 6]]}
+new_features = [5, 7]
 
-print(euclidean_distance)
+for i in dataset:
+    for ii in dataset[i]:
+        plt.scatter(ii[0], ii[1])
+
+
+#[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
+#plt.scatter(new_features[0], new_features[1], s=100, color=i)
+#plt.show()
+
+
+def k_nearest_neighbors(data, predict, k=3):
+    if len(data) >= 3:
+        warnings.warn('K is set to a value less than total voting groups!')
+
+    return vote_result
