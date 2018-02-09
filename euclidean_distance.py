87d9a01b2b61ecf2f361e76a447abb9568e05c27
Manoj Singh Negi <justanothermanoj@gmail.com>
2018-02-09 23:38:17 +0530

    stripped code to write our own k_nearest_neighbors program

--- euclidean_distance.py
+++ euclidean_distance.py
@@ -1,24 +1,7 @@
 import numpy as np
 from math import sqrt
-import matplotlib.pyplot as plt
 import warnings
-from matplotlib import style
 from collections import Counter
-
-style.use('fivethirtyeight')
-
-dataset = {'k': [[1, 2],[2, 3],[3, 1]], 'r': [[6, 5],[7, 7],[8, 6]]}
-new_features = [5, 7]
-
-for i in dataset:
-    for ii in dataset[i]:
-        plt.scatter(ii[0], ii[1])
-
-
-#[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-#plt.scatter(new_features[0], new_features[1], s=100, color=i)
-#plt.show()
-
 
 def k_nearest_neighbors(data, predict, k=3):
     if len(data) >= 3:
@@ -32,12 +15,3 @@ def k_nearest_neighbors(data, predict, k=3):
     votes = [i[1] for i in sorted(distances)[:k]]
     vote_result = Counter(votes).most_common(1)[0][0]
     return vote_result
-
-result = k_nearest_neighbors(dataset, new_features, k=3)
-print(result)
-
-[[plt.scatter(ii[0], ii[1], s=100, color=i) for ii in dataset[i]] for i in dataset]
-plt.scatter(new_features[0], new_features[1], s=100, color=result)
-plt.show()
-
-
